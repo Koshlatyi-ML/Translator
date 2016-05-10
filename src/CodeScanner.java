f9d9403d526eaff0e5cff0616c7300b473b2db2a
Nikolay <ml.koshlatyi@gmail.com>
2016-05-10 16:33:05 +0300

    Changed Map<Integer, String> signature to <String, Integer> because in the time of scanning programm know only spelling of lexeme.

--- src/CodeScanner.java
+++ src/CodeScanner.java
@@ -16,7 +16,7 @@ public class CodeScanner {
 
     private List<Lexeme> lexemeTable;
     private List<String> delimiters;
-    private Map<Integer, String> enterLexemeTable;
+    private Map<String, Integer> enterLexemeTable;
     private File delimitersFile;
     private File exisitingLexemesFile;
 
@@ -26,12 +26,12 @@ public class CodeScanner {
 
         this.lexemeTable = new ArrayList<>();
         this.delimiters = getDelimiters(delimitersFile);
-        this.enterLexemeTable =  getEnterLexemeTable(exisitingLexemesFile);
+        this.enterLexemeTable = getEnterLexemeTable(exisitingLexemesFile);
     }
 
     private List<String> getDelimiters(File delimitersFile) {
         List<String> delimiters = new ArrayList<>();
-        try(Scanner scanner = new Scanner(delimitersFile)) {
+        try (Scanner scanner = new Scanner(delimitersFile)) {
             while (scanner.hasNext()) {
                 delimiters.add(scanner.next());
             }
@@ -41,11 +41,13 @@ public class CodeScanner {
         return delimiters;
     }
 
-    private Map<Integer, String> getEnterLexemeTable(File exisitingLexemesFile) {
-        Map<Integer, String> enterLexemes = new HashMap<>();
-        try(Scanner scanner = new Scanner(exisitingLexemesFile)) {
+    private Map<String, Integer> getEnterLexemeTable(File exisitingLexemesFile) {
+        Map<String, Integer> enterLexemes = new HashMap<>();
+        try (Scanner scanner = new Scanner(exisitingLexemesFile)) {
+            Integer id = scanner.nextInt();
+            String alias = scanner.next();
             while (scanner.hasNext()) {
-                enterLexemes.put(scanner.nextInt(), scanner.next());
+                enterLexemes.put(alias, id);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -54,8 +56,8 @@ public class CodeScanner {
     }
 
     public void getLexems() {
-        try(FileInputStream fIn = new FileInputStream("code.mylang");
-            FileChannel fChan = fIn.getChannel()) {
+        try (FileInputStream fIn = new FileInputStream("code.mylang");
+             FileChannel fChan = fIn.getChannel()) {
 
             MappedByteBuffer mBuf = fChan.map(FileChannel.MapMode.READ_ONLY, 0, fChan.size());
             Charset cs = Charset.forName("UTF-8");
